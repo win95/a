05f97f3812db1652edadf2747edc5db37dd2a247
XMR-miner <ducbk55@gmail.com>
2018-06-28 00:58:14 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,6 @@ minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u ductran9x
 #./cpuminer -t 60 -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u ductran9xhp@gmail.com -p x
 ./xmrig -t 56 -a cryptonight-heavy -o stratum+tcp://pool.loki-pool.com:3333 -u L7UkibE5sRPNuzBaE8Mxk1LRUBGTznMfoJMavACKDVhWFDUYpf3AyPuEQhryognGQKEuXU5GNyxXT6YL6V9kBFkYKKj29iY -p x
 ./xmrig -t 56 -a cryptonight -o stratum+tcp://pool.minexmr.com:7777 -u 47eqqKu3fGYcV272QFggNtN3zRMzMT1ArELr5mdRPGQd6WT24C4M9GAHhsLo9Y9knQBkwHm7R5kHhjmWf5TjWgHEH4GtZYi.user1 -p x
+
+##Turtle
+#TRTLuyhWPxbBuepV7QUXPGYXgqift9i5bCBqzifYqoUFKYffRH9SppYY2fuuF43wpKALTyCb2eRdUH7ryPhRvxaKYQNSfoVRCji
