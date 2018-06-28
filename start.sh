110c5b50426cb121820c4e48116e608fb0db0be3
XMR-miner <ducbk55@gmail.com>
2018-06-28 23:09:12 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,7 @@ minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45560 -u ductran9x
 ./xmrig -t 56 -a cryptonight -o stratum+tcp://pool.minexmr.com:7777 -u 47eqqKu3fGYcV272QFggNtN3zRMzMT1ArELr5mdRPGQd6WT24C4M9GAHhsLo9Y9knQBkwHm7R5kHhjmWf5TjWgHEH4GtZYi.user1 -p x
 
 ##Turtle
-./xmrig -t 32 -a cryptonight-lite -o stratum+tcp://Pool.TRTL.CryptoPool.Space:7777 -u TRTLuyhWPxbBuepV7QUXPGYXgqift9i5bCBqzifYqoUFKYffRH9SppYY2fuuF43wpKALTyCb2eRdUH7ryPhRvxaKYQNSfoVRCji -p x
+./xmrig -t 32 -a cryptonight-lite -o stratum+tcp://Pool.TRTL.CryptoPool.Space:7777 -u TRTLuyhWPxbBuepV7QUXPGYXgqift9i5bCBqzifYqoUFKYffRH9SppYY2fuuF43wpKALTyCb2eRdUH7ryPhRvxaKYQNSfoVRCji -p x --donate-level=1
+
+## BSM
+./xmrig -t 30 -a cryptonight-lite -o stratum+tcp://Pool.BSM.CryptoPool.Space:7777 -u Sm4pAAzN81qTaQPZH2f81nRhXCbx4kK5CRHwr9YQVVKLYeoaCochqN91BdYLYkFUQ9KPePpjYbkt7hA2ESPRR6rD1WhiTmFNb -p x --donate-level=1
